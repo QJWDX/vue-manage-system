8feec759f7e3dd60224b4723c4b1976ff9273263
lizhixiong <lizhixiong@runone.com>
2020-10-23 18:01:31 +0800

    mqtt修改

--- src/components/page/Mqtt.vue
+++ src/components/page/Mqtt.vue
@@ -11,7 +11,8 @@
               :header-cell-style="rowClass"
               @selection-change="handleSelectionChange"
           >
-              <el-table-column prop="content" align="center" label="消息内容"></el-table-column>
+              <el-table-column prop="message" align="center" label="消息内容"></el-table-column>
+              <el-table-column prop="date" align="center" label="发送时间"></el-table-column>
           </el-table>
         </div>
     </div>
@@ -22,17 +23,17 @@
   const options = {
     connectTimeout: 40000,
     clientId: 'myclientid_' + parseInt(Math.random() * 100, 10),
-    username: 'test',
-    password: 'test123',
+    username: 'admin',
+    password: 'admin123',
     clean: true
   };
-  client = mqtt.connect('ws://192.168.1.185:15675/ws', options);
+  client = mqtt.connect('ws://127.0.0.1:15675/ws', options);
   export default {
     data() {
       return {
         componentName: 'menus',
         tableData: [
-          {content : '这是一条mqtt消息！'}
+          
         ]
       }
     },
@@ -57,19 +58,18 @@
       mqttMsg() {
         client.on('connect', (e) => {
           console.log("连接成功！！！")
-          client.subscribe('test_mqtt', { qos: 1 }, (error) => {
+          client.subscribe('message', {qos:1}, (error) => {
             if (!error) {
               console.log('订阅成功')
             } else {
               console.log('订阅失败')
             }
           })
-
         })
         // 接收消息处理
         client.on('message', (topic, message) => {
-          console.log('收到来自', topic, '的消息', message.toString())
-          this.tableData.push = {content : message.toString()};
+          // console.log('收到来自', topic, '的消息', message.toString());
+          this.tableData.push(JSON.parse(message.toString()));
         })
       }
     }
