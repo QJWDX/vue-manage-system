d44fc22db7c313d84ce71be5fe7d592c9c09b96f
lizhixiong <lizhixiong@runone.com>
2020-12-08 17:45:02 +0800

    111

--- src/store/modules/premission.js
+++ src/store/modules/premission.js
@@ -64,10 +64,17 @@ const mutations = {
 };
 
 const actions = {
-    userLoginSuccess(context, data){
-        context.commit('setUserInfo', data.user);
-        context.commit('setToken', data.token);
-        context.commit('setUserAvatar', data.user.avatar);
+    userLogin(context, res){
+        return new Promise(function(resolve, reject){
+            try{
+                context.commit('setUserInfo', res,data.user);
+                context.commit('setToken', res.data.token);
+                context.commit('setUserAvatar', res.data.user.avatar);
+                resolve(res.message);
+            }catch{
+                reject(err);
+            }
+        });
     },
     addMenuData(context, role){
         return new Promise(function(resolve, reject){
