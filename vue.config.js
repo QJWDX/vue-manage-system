dda6fb14485cf3fff95c6c2b4f44a34ec54fd65a
lizhixiong <lizhixiong@runone.com>
2020-12-30 17:58:39 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,40 @@
+const path = require("path");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+ 
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const name = "后台框架";
 module.exports = {
     baseUrl: './',
     assetsDir: 'static',
     productionSourceMap: false,
+    lintOnSave: process.env.NODE_ENV === "development",
+    configureWebpack: {
+        name: name,
+        resolve: {
+            alias: {
+                "@": resolve("src"),
+                "@i": resolve("src/api"),
+                "@c": resolve("src/components"),
+                "@v": resolve("src/pages"),
+                "@s": resolve("src/static"),
+                "@u": resolve("src/utils")
+            }
+        },
+        plugins: [
+            new CompressionWebpackPlugin({
+                filename: "[path].gz[query]",
+                algorithm: "gzip",
+                test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"), //匹配文件名
+                threshold: 10240, //对10K以上的数据进行压缩
+                minRatio: 0.8,
+                deleteOriginalAssets: false //是否删除源文件
+            })
+        ]
+    },
     devServer: {
         proxy: {
             '/api':{
